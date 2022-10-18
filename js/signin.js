81974d77ae52f8fe10223d386103ce0ec40b2da2
ignazioconti <ignazioconti@gmail.com>
2022-10-18 22:57:09 +0200

    add_singin_e_login

--- js/signin.js
+++ js/signin.js
@@ -1,15 +1,28 @@
-const form = document.getElementById('form');
+const form = document.getElementById("form");
 
-function formData (event, form) {
-    event.preventDefault();
-    const data = new FormData (form);
-    const person = {};
-    data.forEach((value, key) => {
-        person[key] = value;
-    })
-    return person;
+function formData(event, form) {
+  event.preventDefault();
+  const data = new FormData(form);
+  const person = {};
+  data.forEach((value, key) => {
+    person[key] = value;
+  });
+  return JSON.stringify(person);
 }
 
-form.addEventListener('submit', (event) => {
-    
-})
+form.addEventListener("submit", (event) => {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: formData(event, form),
+    redirect: "follow",
+  };
+
+  fetch(
+    "https://api-nodejs-todolist.herokuapp.com/user/register",
+    requestOptions
+  ).catch((error) => console.log("error", error));
+});
