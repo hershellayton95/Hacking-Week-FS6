15ad391f787bfe2eab623786007c385d783bed66
d-94k <86233674+d-94k@users.noreply.github.com>
2022-10-18 22:25:15 +0200

    added signin

---
+++ js/signin.js
@@ -0,0 +1,15 @@
+const form = document.getElementById('form');
+
+function formData (event, form) {
+    event.preventDefault();
+    const data = new FormData (form);
+    const person = {};
+    data.forEach((value, key) => {
+        person[key] = value;
+    })
+    return person;
+}
+
+form.addEventListener('submit', (event) => {
+    
+})
