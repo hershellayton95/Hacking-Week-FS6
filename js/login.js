8fd36403c64cb9fc40b1f0ccc1dd53cc10f78052
ignazioconti <ignazioconti@gmail.com>
2022-10-20 16:16:29 +0200

    sistemazione_codice_js

--- js/login.js
+++ js/login.js
@@ -1,50 +1,38 @@
-import {alertMessage, shake, fetchRequest} from "./utils.js";
+import { alertMessage, shake, fetchRequest, formData } from "./utils.js";
 
 //controllo iniziale
-const token = sessionStorage.getItem('token');
+const token = sessionStorage.getItem("token");
 
-if(token){
+if (token) {
   window.location.pathname = "/user/profile.html";
 }
 
 //codice
 const form = document.getElementById("form");
 
-function formData(event, form) {
-  event.preventDefault();
-  const data = new FormData(form);
-  const person = {};
-  data.forEach((value, key) => {
-    person[key] = value;
-  });
-  return JSON.stringify(person);
-}
-
 form.addEventListener("submit", (event) => {
-
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
 
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: formData(event, form),
+    redirect: "follow",
+  };
 
-
-const requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: formData(event, form),
-  redirect: 'follow'
-};
-
-
-fetchRequest("https://api-nodejs-todolist.herokuapp.com/user/login", requestOptions)
-  .then(result => {
-    if(result?.token){
-      sessionStorage.setItem('token', result.token)
-      window.location.pathname = '/user/profile.html';
-    }
-  })
-  .catch((error) => {
-    alertMessage(".message", error);
-    shake('.container-fluid');
-  }); 
+  fetchRequest(
+    "https://api-nodejs-todolist.herokuapp.com/user/login",
+    requestOptions
+  )
+    .then((result) => {
+      if (result?.token) {
+        sessionStorage.setItem("token", result.token);
+        window.location.pathname = "/user/profile.html";
+      }
+    })
+    .catch((error) => {
+      alertMessage(".message", error);
+      shake(".container-fluid");
+    });
 });
-
--- js/utils.js
+++ js/utils.js
@@ -1,36 +1,41 @@
-export function alertMessage (elClass, error) {
+export function alertMessage(elClass, error) {
   const alert = document.querySelector(elClass);
-    alert.innerHTML = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+  alert.innerHTML = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
     <strong>Holy guacamole!</strong> ${error.message}
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>`;
 }
 
-export function shake (elementClass) {
+export function shake(elementClass) {
   const shake = document.querySelector(elementClass);
-  shake.classList.add('shake');
-    setTimeout(() => {
-      shake.classList.remove('shake');
-    }, 1000);
+  shake.classList.add("shake");
+  setTimeout(() => {
+    shake.classList.remove("shake");
+  }, 1000);
 }
 
+export function formData(event, form) {
+  event.preventDefault();
+  const data = new FormData(form);
+  const person = {};
+  data.forEach((value, key) => {
+    person[key] = value;
+  });
+  return JSON.stringify(person);
+}
 
-export async function fetchRequest(url, request){
+export async function fetchRequest(url, request) {
   const response = await fetch(url, request);
   const result = "";
-  
-  if(response.ok){
+
+  if (response.ok) {
     result = await response.json();
   } else {
-    result = await response.json()
-    .then((error) => {
+    result = await response.json().then((error) => {
       throw new Error(error);
-    })
+    });
   }
   return result;
 }
 
-
 export default () => console.log("utils.js");
-
-
